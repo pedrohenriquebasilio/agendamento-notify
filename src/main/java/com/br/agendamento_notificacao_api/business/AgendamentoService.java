9b5898ffde98c112f84fa3f6755e6df1b5c5e73f
Pedro Basilio <phbasilio6903@gmail.com>
2025-03-02 20:15:40 -0300

    Feat: ADD SERVICES

--- src/main/java/com/br/agendamento_notificacao_api/business/AgendamentoService.java
+++ src/main/java/com/br/agendamento_notificacao_api/business/AgendamentoService.java
@@ -1,6 +1,9 @@
 package com.br.agendamento_notificacao_api.business;
 
 
+import com.br.agendamento_notificacao_api.business.mapper.IAgendamentoMapper;
+import com.br.agendamento_notificacao_api.controller.dto.AgendamentoRequestDto;
+import com.br.agendamento_notificacao_api.controller.dto.AgendamentoResponseOut;
 import com.br.agendamento_notificacao_api.infrastructure.repositories.AgendamentoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,5 +13,11 @@ import org.springframework.stereotype.Service;
 public class AgendamentoService {
 
         private final AgendamentoRepository repository;
+        private final IAgendamentoMapper agendamentoMapper;
+
+        public AgendamentoResponseOut gravarAgendamento(AgendamentoRequestDto agendamento){
+               return agendamentoMapper.forResponse(repository.save(agendamentoMapper.forEntity(agendamento)));
+
+        }
 
 }
--- src/main/java/com/br/agendamento_notificacao_api/controller/dto/AgendamentoResponseOut.java
+++ src/main/java/com/br/agendamento_notificacao_api/controller/dto/AgendamentoResponseOut.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 
 import java.time.LocalDateTime;
 
-public record AgendamentoRecordOut(Long id, String emailDestinatario, String telefoneDestinatario, String mensagem,
-                                   @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+public record AgendamentoResponseOut(Long id, String emailDestinatario, String telefoneDestinatario, String mensagem,
+                                     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
                                    LocalDateTime dataHoraEnvio, StatusNotificacaoEnum statusNotificacaoEnum) {
 }
