8151675d58826fb43f664c35a0208e2bf25eddba
Pedro Basilio <phbasilio6903@gmail.com>
2025-03-02 20:41:47 -0300

    Feat: add a new Services for more endpoints in Controller

--- src/main/java/com/br/agendamento_notificacao_api/business/AgendamentoService.java
+++ src/main/java/com/br/agendamento_notificacao_api/business/AgendamentoService.java
@@ -4,9 +4,13 @@ package com.br.agendamento_notificacao_api.business;
 import com.br.agendamento_notificacao_api.business.mapper.IAgendamentoMapper;
 import com.br.agendamento_notificacao_api.controller.dto.AgendamentoRequestDto;
 import com.br.agendamento_notificacao_api.controller.dto.AgendamentoResponseOut;
+import com.br.agendamento_notificacao_api.infrastructure.entities.Agendamento;
+import com.br.agendamento_notificacao_api.infrastructure.exception.NotFoundException;
 import com.br.agendamento_notificacao_api.infrastructure.repositories.AgendamentoRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Service
@@ -18,6 +22,17 @@ public class AgendamentoService {
         public AgendamentoResponseOut gravarAgendamento(AgendamentoRequestDto agendamento){
                return agendamentoMapper.forResponse(repository.save(agendamentoMapper.forEntity(agendamento)));
 
+        }
+
+        public AgendamentoResponseOut buscarAgendamentosPorId(Long id){
+                return agendamentoMapper.forResponse(repository.findById(id).orElseThrow(()-> new NotFoundException("ID NOT FOUND")));
+        }
+
+        public  void  CancelarAgendamento(Long id){
+              Agendamento agendamento = repository.findById(id).orElseThrow(()-> new NotFoundException("Id não encontrado"));
+              repository.save(
+                      agendamentoMapper.forEntityCancelamento(agendamento)
+              );
         }
 
 }
